af779b87cfb456bbaad32ee891691aafa2ca79ce
sufyanali <sufyanalimir313@gmail.com>
2022-03-04 16:17:24 +0530

    display form on age verification

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 let userAge = document.getElementById("age");
 let verifyMessage = document.querySelector(".verifyMessage");
 let ageVerifyBtn = document.getElementById("btn-age-verify");
+let formSection = document.querySelector(".form-section");
+
+const states = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Maharashtra",
+  "Punjab",
+  "Uttar Pradesh",
+];
+
+let stateOptions = "";
+for (var i = 0; i < states.length; i++) {
+  stateOptions += '<option value="' + states[i] + '" />';
+}
+document.getElementById("statesList").innerHTML = stateOptions;
+
+const cities = [
+  "Akola",
+  "Amravati",
+  "Beed",
+  "Bhandara",
+  "Nagpur",
+  "Nanded",
+  "Parbhani",
+  "Jalna",
+  "Aurangabad",
+];
+
+let cityOptions = "";
+for (var i = 0; i < cities.length; i++) {
+  cityOptions += '<option value="' + cities[i] + '" />';
+}
+document.getElementById("citiesList").innerHTML = cityOptions;
 
 function inputLength() {
   if (userAge.value.length > 0) {
@@ -14,6 +51,7 @@ function verifyAge() {
   if (inputLength() > 0) {
     if (userAge.value >= 18) {
       verifyMessage.textContent = "Congrats! You are eligible.";
+      formSection.style.display = "block";
     } else {
       verifyMessage.textContent = "Sorry! You are under age.";
     }
